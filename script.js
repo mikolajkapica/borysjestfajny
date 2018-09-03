d0d5788379b88173585a9a41996c1a83e1c5b8c9
Your Name <you@example.com>
2018-09-03 19:21:12 +0200

    fd

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-$('.image').each(function(i){
-  setTimeout(function(){
-    $('.image').eq(i).addClass('load');
-  }, 200 * i);
+$( document ).ready(function() {
+  $('.image').each(function(i){
+    setTimeout(function(){
+      $('.image').eq(i).addClass('load');
+    }, 200 * i);
+  });
 });
